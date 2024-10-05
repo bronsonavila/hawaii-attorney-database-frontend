16eb03c5daedc63758d69813a1100d2573859d14
Bronson Avila <bronson.avila@gmail.com>
2024-10-04 14:41:59 -1000

    Modify JSON-LD and index.html meta tags

--- src/utils/generateJsonLd.ts
+++ src/utils/generateJsonLd.ts
@@ -1,11 +1,25 @@
 export const generateJsonLd = () => {
   const jsonLd = {
     '@context': 'http://schema.org',
-    '@type': 'WebPage',
-    author: { '@type': 'Person', name: 'Bronson Avila' },
+    '@type': 'WebSite',
+    alternativeHeadline:
+      'Comprehensive Directory of Licensed Attorneys in Hawaii: Insights for Legal Professionals, Researchers, Businesses, Government, and the General Public',
+    audience: {
+      '@type': 'Audience',
+      audienceType: 'Legal Professionals, Researchers, Businesses, Government, General Public'
+    },
+    author: { '@type': 'Person', name: 'Bronson Avila', url: 'https://www.bronsonavila.com/' },
+    citation: { '@type': 'CreativeWork', name: 'Hawaii State Bar Association', url: 'https://hsba.org/' },
+    creativeWorkStatus: 'Published',
     dateModified: new Date().toISOString(),
-    description: 'A searchable and filterable database of attorneys licensed in Hawaii.',
+    description:
+      'A comprehensive, searchable database of attorneys licensed to practice in Hawaii. Useful for research and analysis by legal professionals, businesses, journalists, government agencies, and the general public seeking information on attorneys in Hawaii.',
+    educationalUse: 'Research, Market Analysis, Compliance',
     inLanguage: 'en-US',
+    isAccessibleForFree: true,
+    keywords:
+      'Hawaii attorneys, legal database, lawyer directory, legal research, Hawaii bar, public directory, attorney information',
+    maintainer: { '@type': 'Person', name: 'Bronson Avila', url: 'https://www.bronsonavila.com/' },
     name: 'Hawaii Attorney Database',
     url: 'https://www.hawaiiattorneydatabase.com/'
   }
