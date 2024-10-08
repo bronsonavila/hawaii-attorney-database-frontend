62f722f23e4929e8d1e0148b04a5051b9a20867a
Bronson Avila <bronson.avila@gmail.com>
2024-10-08 01:14:14 -1000

    Modify mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,16 @@ const DATA_GRID_THEME_OVERRIDES = {
       menu: { '.MuiButtonBase-root': { minHeight: 'auto' } },
       panelWrapper: {
         '@media (max-width: 599px)': {
-          '&:has(.MuiDataGrid-panelContent)': { width: 'clamp(370px, 95vw, 540px)' },
-          '.MuiButton-icon': { ml: '-2px' },
-          '.MuiButtonBase-root': { fontSize: 13, padding: '4px 5px' },
-          '.MuiDataGrid-panelFooter .MuiSvgIcon-root': { fontSize: 18 },
-          '.MuiFormLabel-root': { transform: 'translate(0, -1.5px) scale(0.67)' },
-          '.MuiInputBase-input': { fontSize: 13 },
-          '.MuiInputBase-root': { fontSize: 13 },
-          '.MuiSvgIcon-root': { fontSize: 16 }
+          '&:has(.MuiDataGrid-panelContent)': {
+            width: 'clamp(370px, 95vw, 540px)',
+            '.MuiButton-icon': { ml: '-2px' },
+            '.MuiButtonBase-root': { fontSize: 13, padding: '4px 5px' },
+            '.MuiDataGrid-panelFooter .MuiSvgIcon-root': { fontSize: 18 },
+            '.MuiFormLabel-root': { transform: 'translate(0, -1.5px) scale(0.67)' },
+            '.MuiInputBase-input': { fontSize: 13 },
+            '.MuiInputBase-root': { fontSize: 13 },
+            '.MuiSvgIcon-root': { fontSize: 16 }
+          }
         }
       },
       root: { border: 'none', height: '100dvh', width: '100dvw' }
