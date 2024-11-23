fab10a07d9f0827f9b16df4f81d8576d7b19a0c1
Bronson Avila <bronson.avila@gmail.com>
2024-11-22 14:10:46 -1000

    Update processed members CSV

--- src/components/AboutDialogContent.tsx
+++ src/components/AboutDialogContent.tsx
@@ -34,7 +34,7 @@ export const AboutDialogContent: FC<AboutDialogContentProps> = ({ onClose, onFee
         <Stack spacing={1.5}>
           <Typography variant="body2">Independently maintained by: {SITE_METADATA.author.name}</Typography>
 
-          <Typography variant="body2">Last updated: Nov. 3, 2024</Typography>
+          <Typography variant="body2">Last updated: Nov. 17, 2024</Typography>
         </Stack>
       </DialogContent>
 
