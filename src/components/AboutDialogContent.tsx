9e0903cc2c19b4f595377ce241aef4e01b5079d8
Bronson Avila <bronson.avila@gmail.com>
2025-12-16 14:41:30 -1000

    Update member records

--- src/components/AboutDialogContent.tsx
+++ src/components/AboutDialogContent.tsx
@@ -38,7 +38,7 @@ export const AboutDialogContent = ({ onClose, onFeedbackMode }: AboutDialogConte
             Independently maintained by: <ExternalLink href={METADATA.author.url}>{METADATA.author.name}</ExternalLink>
           </Typography>
 
-          <Typography variant="body2">Last updated: December 15, 2025</Typography>
+          <Typography variant="body2">Last updated: December 16, 2025</Typography>
         </Stack>
       </DialogContent>
 
